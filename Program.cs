14b3cd82adf21fc8137c9cde433cdf9e44009b3c
kt1ztr <kata1u3atop@gmail.com>
2022-12-25 13:17:57 +0300

    proga

--- Program.cs
+++ Program.cs
@@ -1,7 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-asdddddddddddddd
+﻿Console.Clear();
+    Console.Write("Укажите количество элементов массива ");
+    int len = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[len];
+    FillArray(array);
+    Console.Write("Введенный массив -------> ");
+    PtintArray(array);
+    Console.Write("Обработанный массив, состоящий из элементов <|= трем символам --> ");
+    PtintArray(Array3symb(array));         
+void FillArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите значение ({i+1} элемента)..");
+        array[i] = Console.ReadLine();
+    }
+}
+   void PtintArray(string[] array)
+{
+    int i = 0;
+    for (i = 0; i < array.Length-1; i++)  
+    {
+        Console.Write(array[i] + " / ");
+    }
+    Console.WriteLine(array[i]);
+}
 
-asdasdasd
-
-
+string[] Array3symb(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)  
+    {
+        if (array[i].Length < 4)
+        {
+            array[count] = array[i];
+            count++;
+        }
+    }
+    string[] array_res = new string[count];
+    for (int j = 0; j < count; j++) 
+    {
+        array_res[j] = array[j];
+    }
+    return array_res;
+}
